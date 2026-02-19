73c081b1dc5aa0a143622fc4720c05dd18e68f7f
Kayky Freitas <kayky_defreitas@hotmail.com>
2026-02-18 23:39:12 -0300

    feat: rerun

--- app/internal/port/storage_port.go
+++ app/internal/port/storage_port.go
@@ -8,7 +8,9 @@ import (
 type StoragePort interface {
 	GetObject(ctx context.Context, bucket, key string) (io.ReadCloser, error)
 
+	
 	PutObject(ctx context.Context, bucket, key string, body io.Reader) (string, error)
+
 
 	DeleteObject(ctx context.Context, bucket, key string) error
 }

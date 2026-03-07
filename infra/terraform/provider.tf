c8a4e5855db4f7de0c003e518f7f7ced36b6be95
Kayky Freitas <kayky_defreitas@hotmail.com>
2026-03-07 14:42:27 -0300

    feat: ajuste envs

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -2,23 +2,23 @@ provider "aws" {
   region = "sa-east-1"
 }
 
-data "aws_eks_cluster" "fastfood" {
-  name = "fastfood-soat-terraform"
+data "aws_eks_cluster" "hackaton" {
+  name = "hackaton-soat-terraform"
 }
 
-data "aws_eks_cluster_auth" "fastfood" {
-  name = "fastfood-soat-terraform"
+data "aws_eks_cluster_auth" "hackaton" {
+  name = "hackaton-soat-terraform"
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.fastfood.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.fastfood.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.fastfood.token
+  host                   = data.aws_eks_cluster.hackaton.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.hackaton.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.hackaton.token
 }
 
 provider "kubectl" {
-  host                   = data.aws_eks_cluster.fastfood.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.fastfood.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.fastfood.token
+  host                   = data.aws_eks_cluster.hackaton.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.hackaton.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.hackaton.token
   load_config_file       = false
 }
